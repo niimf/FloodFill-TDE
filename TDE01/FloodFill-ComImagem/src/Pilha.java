5a304cf5132c765a765359f6c0ef239d2091a13e
Felipe Brugnera <165804740+lipebrug@users.noreply.github.com>
2024-10-07 20:52:24 -0300

    Update Pilha.java

--- TDE01/FloodFill-ComImagem/src/Pilha.java
+++ TDE01/FloodFill-ComImagem/src/Pilha.java
@@ -1,45 +1,39 @@
 public class Pilha<T> {
 
-    private int top;  // Índice do topo da pilha
-    private T[] data; // Array que armazena os elementos da pilha
+    private int top;  
+    private T[] data; 
 
-    // Construtor que define o tamanho da pilha
     public Pilha(int size) {
-        top = -1; // A pilha começa vazia, então o topo é -1
-        data = (T[]) new Object[size]; // Criamos um array de tamanho definido
+        top = -1; 
+        data = (T[]) new Object[size]; 
     }
 
-    // Método para adicionar um elemento na pilha
     public void push(T value) {
         if (isFull()) {
             System.out.println("Stack is full");
         } else {
-            data[++top] = value; // Incrementa o topo e armazena o valor
+            data[++top] = value; 
         }
     }
 
-    // Método para remover o elemento do topo da pilha
     public T pop() {
         if (isEmpty()) {
             System.out.println("Stack is empty");
             return null;
         } else {
-            return data[top--]; // Retorna o elemento e decrementa o topo
+            return data[top--]; 
         }
     }
 
-    // Método para limpar a pilha
     public void clear() {
-        top = -1; // Definimos o topo como -1, o que "reseta" a pilha
+        top = -1; 
     }
 
-    // Verifica se a pilha está cheia
     public boolean isFull() {
-        return top == data.length - 1; // A pilha está cheia quando o topo é o último índice
+        return top == data.length - 1; 
     }
 
-    // Verifica se a pilha está vazia
     public boolean isEmpty() {
-        return top == -1; // A pilha está vazia quando o topo é -1
+        return top == -1; 
     }
 }
